2078a0e2e91a67f04c133b7204f2b6f1235764eb
Scott Schreckengaust <scottschreckengaust@users.noreply.github.com>
2024-11-21 15:05:03 -0800

    feat: validation testing
    Signed-off-by: Scott Schreckengaust <scottschreckengaust@users.noreply.github.com>

--- samples/bedrock-agent/outputs.tf
+++ samples/bedrock-agent/outputs.tf
@@ -1,20 +1,14 @@
+output "s3_uri" {
+  description = "The S3 Data source bucket name."
+  value       = module.bedrock.s3_data_source_arn != null ? join("", concat(["s3://", split(":", module.bedrock.s3_data_source_arn)[length(split(":", module.bedrock.s3_data_source_arn)) - 1]])) : null
+}
 
 output "knowledge-base-id" {
   description = "The Bedrock Agent's first knowledge base"
   value       = length(module.bedrock.bedrock_agent) > 0 ? length(module.bedrock.bedrock_agent[0].knowledge_bases) > 0 ? module.bedrock.bedrock_agent[0].knowledge_bases[0].knowledge_base_id : null : null
 }
 
-output "default_kb_identifier" {
-  description = "The Bedrock Agent's default knowledge base"
-  value       = module.bedrock.default_kb_identifier
-}
-
-output "s3_uri" {
-  description = "The S3 Data source bucket name."
-  value       = module.bedrock.s3_data_source_arn != null ? join("", concat(["s3://", split(":", module.bedrock.s3_data_source_arn)[length(split(":", module.bedrock.s3_data_source_arn)) - 1]])) : null
-}
-
-output "bedrock_agent" {
-  description = "valu"
-  value       = length(module.bedrock.bedrock_agent) > 0 ? module.bedrock.bedrock_agent[0].agent_id : null
+output "data-source-id" {
+  description = "The Bedrock Agent's data source"
+  value       = module.bedrock.datasource_identifier
 }
