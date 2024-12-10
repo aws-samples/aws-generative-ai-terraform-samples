a584e3cf920135feacc58e59d4a87b988b0b2f2c
Scott Schreckengaust <scottschreckengaust@users.noreply.github.com>
2024-12-10 11:17:54 -0800

    wip: testing document ingestion module 1.0.1
    Signed-off-by: Scott Schreckengaust <scottschreckengaust@users.noreply.github.com>

--- samples/document-explorer/terraform-config-backend/main.tf
+++ samples/document-explorer/terraform-config-backend/main.tf
@@ -1,7 +1,9 @@
 module "genai_doc_ingestion" {
   #checkov:skip=CKV_TF_1:Terraform registry has no ability to use a commit hash
-  source             = "aws-ia/genai-document-ingestion-rag/aws"
-  version            = "1.0.0"
+  # source             = "aws-ia/genai-document-ingestion-rag/aws"
+  # version            = "1.0.1"
+  source             = "github.com/aws-ia/terraform-aws-genai-document-ingestion-rag//?ref=4137b7ede3b80c11d28658877c97b3fef22cf31e"
+
   solution_prefix    = "doc-explorer"
   container_platform = "linux/amd64"
   force_destroy      = true
