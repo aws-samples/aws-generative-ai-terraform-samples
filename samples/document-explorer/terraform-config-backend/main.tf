3e6e3558ecc7a688d742f339084d20a9447015e9
Scott Schreckengaust <scottschreckengaust@users.noreply.github.com>
2024-12-11 07:38:40 -0800

    feat: bump versions for modules
    Signed-off-by: Scott Schreckengaust <scottschreckengaust@users.noreply.github.com>

--- samples/document-explorer/terraform-config-backend/main.tf
+++ samples/document-explorer/terraform-config-backend/main.tf
@@ -1,8 +1,7 @@
 module "genai_doc_ingestion" {
   #checkov:skip=CKV_TF_1:Terraform registry has no ability to use a commit hash
-  # source             = "aws-ia/genai-document-ingestion-rag/aws"
-  # version            = "1.0.1"
-  source             = "github.com/aws-ia/terraform-aws-genai-document-ingestion-rag//?ref=4137b7ede3b80c11d28658877c97b3fef22cf31e"
+  source             = "aws-ia/genai-document-ingestion-rag/aws"
+  version            = "1.0.1"
 
   solution_prefix    = "doc-explorer"
   container_platform = var.container_platform
